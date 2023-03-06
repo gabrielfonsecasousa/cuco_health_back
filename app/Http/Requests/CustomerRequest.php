41b9915ad9c188bc40476e65e0a2361768a9499e
“Your <gabrielbromo@outlook.com>
2023-03-06 18:17:07 -0300

    create request validation

--- app/Http/Requests/CustomerRequest.php
+++ app/Http/Requests/CustomerRequest.php
@@ -7,26 +7,42 @@ use Illuminate\Foundation\Http\FormRequest;
 class CustomerRequest extends FormRequest
 {
     /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
+     * Determine if the user is authorized to make this request.
      */
-    public function toArray($request)
+    public function authorize(): bool
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'cpf' => $this->cpf,
-            'birthday' => $this->birthday,
-            'phone' => $this->phone,
-        ];
+        return true;
     }
 
-    public function with($request)
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
     {
-        return [
-            'message' => 'Customer successfully recovered.',
-            'status' => 200,
-        ];
+        $id = $this->route('customer');
+        $nameRules = ['required', 'string', 'max:255'];
+        $cpfRules = ['required', '', 'string', 'max:14'];
+        $birthdayRules = ['required', 'date'];
+        $phoneRules = ['required', 'string', 'max:20'];
+        switch ($this->method()) {
+            case 'POST':
+                return [
+                    'name' => $nameRules,
+                    'cpf' => array_merge($cpfRules, ['unique:customers']),
+                    'birthday' => $birthdayRules,
+                    'phone' => $phoneRules,
+                ];
+            case 'PUT':
+                return [
+                    'name' => $nameRules,
+                    'cpf' => array_merge($cpfRules, ['unique:customers,cpf,' . $id]),
+                    'birthday' => $birthdayRules,
+                    'phone' => $phoneRules,
+                ];
+            default:
+                throw new \InvalidArgumentException("HTTP method not supported");
+        }
     }
 }
